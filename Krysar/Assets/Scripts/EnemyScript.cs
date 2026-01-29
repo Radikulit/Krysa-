c4204206cfe9b75f190dcdcbfd6d21b185a4ddff
Radikulit <lradomir08@gmail.com>
2026-01-29 10:27:58 +0100

    Damage Text pridan

--- Krysar/Assets/Scripts/EnemyScript.cs
+++ Krysar/Assets/Scripts/EnemyScript.cs
@@ -5,14 +5,28 @@ using UnityEngine.UI;
 public class EnemyScript : MonoBehaviour
 {
     public TextMeshProUGUI HPCount;
+    public TextMeshProUGUI DecreaseHPText;
     private float enemyHp = 0;
     void Start()
     {
         enemyHp = UnityEngine.Random.Range(10, 16);
-        UpdateHPText();
+        UpdateHPText();   
     }
     public void TakeDamage(float damage)
     {
+        if (damage < 0)//osetreni pro decreased text
+        {
+            damage = 0;
+            DecreaseHPText.gameObject.SetActive(false);
+
+        }
+        else
+        {
+            DecreaseHPText.text = damage.ToString("- 0.0");
+            DecreaseHPText.gameObject.SetActive(true);
+            Invoke(nameof(HideDamageText), 1f);
+        }
+
         enemyHp -= damage;
         if (enemyHp < 0)//osetreni 
         {
@@ -25,5 +39,9 @@ public class EnemyScript : MonoBehaviour
     private void UpdateHPText()
     {
         HPCount.text = enemyHp.ToString(": 0.0");
+    }
+    private void HideDamageText()
+    {
+        DecreaseHPText.gameObject.SetActive(false);
     }
 }
