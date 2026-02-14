c4d70939c3995db98df4d712b061ee0c1f5af471
Radikulit <lradomir08@gmail.com>
2026-02-14 20:01:52 +0100

    Sick organs are now random

--- Krysar/Assets/Scripts/VisorScript.cs
+++ Krysar/Assets/Scripts/VisorScript.cs
@@ -7,9 +7,18 @@ public class VisorScript : MonoBehaviour
     public Button VisorButton;
     public Button DeactivateVisorButton;
 
+    public GameObject[] organs;
+
     public GameObject OrganHolder;
     public GameObject visorCursor;
     public GameObject VisorPanel;
+
+    private int[] sickOrgans = new int[2];
+
+    private void Start()
+    {
+        ChooseSickOrgans();
+    }
     void Update()
     {
         if (!visorCursor.activeSelf) return;
@@ -19,6 +28,25 @@ public class VisorScript : MonoBehaviour
 
         visorCursor.transform.position =
             Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    public void ChooseSickOrgans()
+    {
+        foreach (var organ in organs)
+        {
+            organ.SetActive(false);
+        }
+
+        int a = Random.Range(0, organs.Length);
+        int b = Random.Range(0, organs.Length);
+
+        while (b == a)
+        {
+            b = Random.Range(0, organs.Length);
+        }
+
+        organs[a].SetActive(true);
+        organs[b].SetActive(true);
     }
 
     public void ActivateVisor()
